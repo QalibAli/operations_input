59681c12c674986242a1722075b0d685a3c74d05
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-02-15 09:00:36 +0400

    forth commit

--- new_tasks.dart
+++ new_tasks.dart
@@ -2,4 +2,5 @@
 // Verilmiş 2 ədədin hasilini , cəmini , fərqini , nisbətini və qalığını tapın QEYD: verilmiş ədəd inputa daxil olunan ededdir
 // Ele bir proqram yazin ki, istifadeci ucbucagin 3 terefinin uzunlugun daxil etsin. Sonda program üçbucağın sahəsini tapsin.
 // Yuxaridaki tapsirigi kvadrat fiquru üçündə tətbiq edin
-// İstifadəçi 3 ədəd daxil etsin. Daxil edilən bu ədədlər numbers adlı Listin içinə əlavə olunsun.
+// İstifadəçi 3 ədəd daxil etsin. Daxil edilən bu ədədlər numbers adlı , tipi int olan Listin içinə əlavə olunsun.
+
