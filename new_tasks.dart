f0f26284c06845ebbbd6be2ee4d0133aef5eadb1
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-02-15 09:05:52 +0400

    fivth commit

--- new_tasks.dart
+++ new_tasks.dart
@@ -1,6 +1,6 @@
 // Verilmiş ədədlə 13 arasındakı fərqi əldə etmək üçün proqram yazın. QEYD: verilmiş ədəd inputa daxil olunan ededdir
 // Verilmiş 2 ədədin hasilini , cəmini , fərqini , nisbətini və qalığını tapın QEYD: verilmiş ədəd inputa daxil olunan ededdir
-// Ele bir proqram yazin ki, istifadeci ucbucagin 3 terefinin uzunlugun daxil etsin. Sonda program üçbucağın sahəsini tapsin.
+// Ele bir proqram yazin ki, istifadeci ucbucagin hundurluyunu (h) ve hundurluyu perpendikulyar olan terefi daxil (b) etsin.  Sonda program üçbucağın sahəsini tapsin. qeyd: Ucbucagin düsturu: 1/2x(bxh)
 // Yuxaridaki tapsirigi kvadrat fiquru üçündə tətbiq edin
 // İstifadəçi 3 ədəd daxil etsin. Daxil edilən bu ədədlər numbers adlı , tipi int olan Listin içinə əlavə olunsun.
 
